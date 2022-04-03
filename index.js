2bbe0016679653324a1f053b586e71d9d1c8bd8f
OwethuSotomela <owethusotomela@gmail.com>
2022-04-03 16:24:57 +0200

    Setting up express app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add code in here to create an API with ExpressJS
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 4017;
+app.listen(PORT, function () {
+    console.log(`App started at port: ${PORT}`)
+});
