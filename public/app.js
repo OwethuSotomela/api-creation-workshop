05e4b4f87970d841a92347ee4ccb704f1719f11d
OwethuSotomela <owethusotomela@gmail.com>
2022-04-06 16:00:21 +0200

    Adding values using post

--- public/app.js
+++ public/app.js
@@ -20,11 +20,11 @@ genderOptions.addEventListener('click', function (evt) {
 	filterData();
 });
 
-function myFunction() {
-	var x = document.getElementById("snackbar");
-	x.className = "show";
-	setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
-}
+// function myFunction() {
+// 	var x = document.getElementById("snackbar");
+// 	x.className = "show";
+// 	setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+// }
 
 function filterData() {
 	axios
